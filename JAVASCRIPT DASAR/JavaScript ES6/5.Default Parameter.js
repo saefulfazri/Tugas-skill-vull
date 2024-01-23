fe9fad35442486408c0b93385a355603faf52e1e
Saefu_Fazri <muhammadsaefulfazri1983@gmai.com>
2024-01-23 21:17:05 +0700

    JavaScript ES6

---
+++ JAVASCRIPT DASAR/JavaScript ES6/5.Default Parameter.js
@@ -0,0 +1,5 @@
+// Tulis kode kalian di bawah ini
+function warnaKesukaan(warna = "putih") {
+  return "Warna kesukaan saya adalah " + warna;
+}
+console.log(warnaKesukaan());
---
+++ JAVASCRIPT DASAR/JavaScript ES6/6.Rest Parameter & Spread Operator.js
@@ -0,0 +1,10 @@
+let namaBuah1 = ["apel", "mangga"];
+let namaBuah2 = ["sirsak", "semangka"];
+
+let semuaNamaBuah;
+
+// Tulis kode kalian di bawah ini
+
+semuaNamaBuah = [...namaBuah1, ...namaBuah2];
+
+console.log(semuaNamaBuah);
